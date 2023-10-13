10f0222f259266dcf1965535304a29c4ad9cef9b
lazy <144381160+subconstruction@users.noreply.github.com>
2023-10-13 11:18:12 +0300

    Update dragging.h

--- dragging.h
+++ dragging.h
@@ -10,12 +10,7 @@ class BorderlessWindow : public QWidget
 public:
     BorderlessWindow(QWidget* parent = nullptr) : QWidget(parent)
     {
-        setWindowFlags(Qt::FramelessWindowHint); // Remove window frame (title bar, border)
-
-        // Set a background color (you can customize this)
-        this->setStyleSheet("background-color: lightgray;");
-
-        // You can add other widgets and elements to this window as needed
+        setWindowFlags(Qt::FramelessWindowHint);
     }
 
 protected:
@@ -23,7 +18,6 @@ protected:
     {
         if (event->button() == Qt::LeftButton)
         {
-            // Store the initial mouse position for dragging
             initialMousePos = event->globalPos();
             initialWindowPos = this->pos();
         }
@@ -33,13 +27,11 @@ protected:
     {
         if (event->buttons() & Qt::LeftButton)
         {
-            // Calculate the new window position based on the mouse movement
             int dx = event->globalX() - initialMousePos.x();
             int dy = event->globalY() - initialMousePos.y();
             int newX = initialWindowPos.x() + dx;
             int newY = initialWindowPos.y() + dy;
 
-            // Move the window to the new position
             this->move(newX, newY);
         }
     }
